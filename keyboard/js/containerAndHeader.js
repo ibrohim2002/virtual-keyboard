810d19fa35710bcbc152f770895a64070ccd01f3
ibrohim2002 <74803608+ibrohim2002@users.noreply.github.com>
2022-05-10 03:33:33 +0500

    feat:eslint

--- keyboard/js/containerAndHeader.js
+++ keyboard/js/containerAndHeader.js
@@ -1,13 +1,13 @@
 import Control from './control.js';
 
 class ContainerAndHeader extends Control {
-    constructor(parent) {
-        super(parent, 'div', 'container');
-        this.title = new Control(this.element, 'h1', 'title');
-        this.title.element.innerHTML = 'Virtual Keyboard';
-        this.changeLang = new Control(this.element, 'p', 'description');
-        this.changeLang.element.innerHTML = 'ALT + CTRL to change the language';
-    }
+  constructor(parent) {
+    super(parent, 'div', 'container');
+    this.title = new Control(this.element, 'h1', 'title');
+    this.title.element.innerHTML = 'Virtual Keyboard';
+    this.changeLang = new Control(this.element, 'p', 'description');
+    this.changeLang.element.innerHTML = 'ALT + CTRL to change the language';
+  }
 }
 
 const createContainerAndHeader = () => new ContainerAndHeader(document.body);
